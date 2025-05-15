aace0e27bb8b20769d7975f8926e555d091035b5
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-15 16:18:33 +0300

    - Создание новой папки - Переименование первой папки в String

--- main/String/2.15-validate-email-address.js
+++ main/String/2.15-validate-email-address.js
@@ -8,4 +8,3 @@ let invalidEmail = "artemgmailcom";
 let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 console.log(emailPattern.test(validEmail));
 console.log(emailPattern.test(invalidEmail));
-
