f640a32fec483d1cb65895d5a44e5feb88f13603
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-15 19:24:40 +0300

    Преобразование десятичных значений

---
+++ main/Number/3.6-decimal-to-hex.js
@@ -0,0 +1,10 @@
+// Преобразование десятичных значений
+// в шестнадцатеричные
+// Задача
+// Есть десятичное число, нужно вычислить его шестнадцатеричный эквивалент.
+
+{
+  let decimalNumber = 12854;
+  // eslint-disable-next-line no-unused-vars
+  let hexString = decimalNumber.toString(16);
+}
