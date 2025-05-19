fa008c07dde81bfc2091210519cd87889c8e77ed
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-19 10:05:32 +0300

    Объединение данных с помощью объектных литералов

---
+++ main/Object/7.2-object-merge.js
@@ -0,0 +1,17 @@
+// Объединение данных с помощью объектных литералов
+// Задача
+// Объединить несколько значений в группу, чтобы создать простейший пакет
+// данных
+
+{
+  let personProfile = {
+    name: "Artem",
+    age: 27,
+    address: {
+      country: "Ukraine",
+      city: "Odesa",
+    },
+  };
+
+  console.log(personProfile);
+}
