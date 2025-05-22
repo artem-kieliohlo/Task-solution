c90539378af84b5fe52f899b51d7063eede0fdad
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-22 14:11:06 +0300

    Отладка кода JavaScript

---
+++ main/Browser-Toolkit/11-1-debugging-javascript.js
@@ -0,0 +1,9 @@
+// Отладка кода JavaScript
+// Задача
+// Узнать значение переменной в определенный момент выполнения кода JavaScript.
+
+function normalize(string) {
+  const normalized = string.replace(/[^\w]/g, "").toLowerCase();
+  debugger;
+  return normalized;
+}
