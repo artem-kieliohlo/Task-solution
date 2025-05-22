267928e8383bdf8f17c15c2cb90ea81f231ed6cb
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-22 16:24:40 +0300

    Изменение класса элемента

---
+++ main/DOM/12.7-change-element-class.js
@@ -0,0 +1,6 @@
+// Изменение класса элемента
+// Задача
+// Изменить применяемые к элементу правила CSS, изменив его класс.
+
+let button = document.querySelector(".send");
+button.className = "click";
