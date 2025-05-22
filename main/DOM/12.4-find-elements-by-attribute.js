619164aed6c370d292174a8c0923dc2031373f82
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-22 16:01:27 +0300

    Добавление HTML-документа и скрипта для поиска элементов с заданным атрибутом

---
+++ main/DOM/12.4-find-elements-by-attribute.js
@@ -0,0 +1,6 @@
+// Поиск всех элементов с данным атрибутом
+// Задача
+// Найти в веб-документе все элементы с данным атрибутом.
+
+let elements = document.querySelectorAll("[class]");
+console.log(elements);
