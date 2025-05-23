ffbb6da229e36341c6f97e8a1ea6a8fe156f89b7
Artem-Kelioglo <akielioghlo@icloud.com>
2025-05-23 10:10:58 +0300

    Скрытие частей страницы

---
+++ main/DOM/12.14-hide-page-sections.js
@@ -0,0 +1,8 @@
+// Скрытие частей страницы
+// Задача
+// Временно скрыть существующий элемент страницы и его дочерние элементы.
+
+let h5 = document.querySelector("h5");
+h5.style.visibility = "hidden";
+// или
+h5.style.display = "none";
