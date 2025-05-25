2fcb1e7127f3c3a879a6242718c095e4bafb9dec
Artem-Kieliohlo <akielioghlo@icloud.com>
2025-05-25 09:49:32 +0300

    Сохранение информации в cookies

---
+++ main/Saving Data/14.1-save-info-in-cookies.js
@@ -0,0 +1,6 @@
+// Сохранение информации в cookies
+// Задача
+// Прочитать из cookie браузера несколько значений или записать значения в cookie.
+
+document.cookie = "name=Artem";
+console.log(document.cookie)
