ea6f840c68edda801b8330ccabdf4b8f340adfda
Artem-Kieliohlo <akielioghlo@icloud.com>
2025-05-26 10:44:45 +0300

    Упрощение IndexedDB с помощью библиотеки

---
+++ main/Saving Data/14.5-indexeddb-simplified-with-library.js
@@ -0,0 +1,5 @@
+// Упрощение IndexedDB с помощью библиотеки
+// Задача
+// Наладить асинхронное взаимодействие с IndexedDB при помощи промисов JavaScript.
+
+//Подключить библиотеку IDB (https://github.com/jakearchibald/idb)
